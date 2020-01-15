3199300c970a985610946708ca856f5d63c81b2d
amsterdamAL <amsterdamAL@gmail.com>
2020-01-14 23:47:41 -0500

    fix

--- app/javascript/stuff/motion.js
+++ app/javascript/stuff/motion.js
@@ -1,84 +1,36 @@
+var controller = new ScrollMagic.Controller();
+
 
 $(document).ready(function() {
   
-    //WITH Timelines (cleaner, more versatile)
-    //var tl = gsap.timeline({repeat: 2, repeatDelay: 1});
-    //tl.to(".al", {x: 100, duration: 1});
-    //tl.to(".al", {y: 50, duration: 1});
-    //tl.to(".al", {opacity: 0, duration: 1});
-
-    // then we can control the whole thing easily...
-    //tl.pause();
-    //tl.resume();
-    //tl.seek(1.5);
-    //tl.reverse();
-
-    //gsap.to("#al", {x: 100, duration: 1});
-    //gsap.to("#al", {y: 50, duration: 1, delay: 1});      //wait 1 second
-    //gsap.to("#al", {opacity: 0, duration: 1, delay: 2}); //wait 2 seconds
-
-    //WITH Timelines (cleaner, more versatile)
-    var tl = gsap.timeline({repeat: 3, repeatDelay: 1});
-    //tl.to("#al", {x: 100, duration: 5});
-    //tl.to("#al", {y: 50, duration: 1});
-    //tl.to("#al", {opacity: 0, duration: 1});
-
-    var transformRatio = $('.animation').height()/$('#manos').height();
-    var transformRatiox = $('.animation').width()/$('#manos').width();
-
-    tl.from("#manos", 
-        
-        { yPercent: 100, duration: 1},
-        
-        
-        
-        
-        
-    );
-
-    tl.to("#al", {left: "50%" , duration: 1.5, xPercent: -100, yPercent: -50});
-    tl.to("#nat", {left: "50%" , duration: 1.5, xPercent: -40, yPercent: -60});
     
-    tl.to("#al, #nat, #manos", {opacity: 0, duration: 1});
-    tl.to("#end", {opacity: 1, duration: 1});
-    tl.to("#end", {width: 3000, duration: 3, height: 3000, opacity: 0});
-    
-    
+    //var tl = gsap.timeline({repeat: 3, repeatDelay: 1});
    
-    
-    //tl.to("#manos", 
+
+    //var transformRatio = $('.animation').height()/$('#manos').height();
+    //var transformRatiox = $('.animation').width()/$('#manos').width();
+
+    //tl.from("#manos", 
         
-        //{ xPercent: -100 * transformRatiox, duration: 1}
-        //{ xPercent: 50 * transformRatio + 50, duration: 1}
-        //{ yPercent: -50 * transformRatio - 50, duration: 1}
-        
-        
-        
+    //    { yPercent: 100, duration: 1},
         
     //);
 
-    //tl.to("#manos", 
+    //tl.to("#al", {left: "50%" , duration: 1.5, xPercent: -100, yPercent: -50});
+    //tl.to("#nat", {left: "50%" , duration: 1.5, xPercent: -40, yPercent: -60});
+    
+    //tl.to("#al, #nat, #manos", {opacity: 0, duration: 1});
+    //tl.to("#end", {opacity: 1, duration: 1});
+    //tl.to("#end", {width: 3000, duration: 3, height: 3000, opacity: 0});
+    
 
-        
-        //{width: 5000, duration: 2 }
-        
-        //{ xPercent: -100 * transformRatiox, duration: 1}
-        //{ xPercent: 50 * transformRatio + 50, duration: 1}
-        //{ yPercent: -50 * transformRatio - 50, duration: 1}
-        
-        
-        
-        
-  //  );
-
-     
+    var scene = new ScrollMagic.Scene({
+        triggerElement: "#trigger1"
+    })
+    .setTween("#animate1", 0.5, {scale: 8.5}) // trigger a TweenMax.to tween
+    .addIndicators({name: "1 (duration: 0)"}) // add indicators (requires plugin)
+    .addTo(controller);
     
    
-
-   // tl.to("#manos", {transform: 'translateX(100vw)', duration: 2});
-
-    //alert("wtf");
-
-    //tl.resume();
     
 });
