e9b3002e4ce01a3970fb9e77d84796d9abb94ed3
amsterdamAL <amsterdamAL@gmail.com>
2020-01-15 12:13:45 -0500

    fix

--- app/javascript/stuff/motion.js
+++ app/javascript/stuff/motion.js
@@ -4,7 +4,7 @@ var controller = new ScrollMagic.Controller();
 $(document).ready(function() {
   
     
-    var tl = gsap.timeline({repeat: 3, repeatDelay: 1});
+    var tl = gsap.timeline();
    
 
     //var transformRatio = $('.animation').height()/$('#manos').height();
@@ -16,22 +16,25 @@ $(document).ready(function() {
         
     //);
 
-    //tl.to("#al", {left: "50%" , duration: 1.5, xPercent: -100, yPercent: -50});
-    //tl.to("#nat", {left: "50%" , duration: 1.5, xPercent: -40, yPercent: -60});
+    tl.to("#al", {left: "50%", xPercent: -100, duration: 3.7}, 0);
+    tl.to("#nat", {right: "50%", xPercent: 50, duration: 3.6}, 0);
     
-    //tl.to("#al, #nat, #manos", {opacity: 0, duration: 1});
-    //tl.to("#end", {opacity: 1, duration: 1});
-    //tl.to("#end", {width: 3000, duration: 3, height: 3000, opacity: 0});
+    //tl.to("#al, #nat", {opacity: 0, duration: 1.5}, 2);
+    tl.to("#end", {opacity: 1, duration: 1.5});
+    tl.to("#end", {width: 3000, duration: 3, height: 3000, opacity: 0});
+    tl.to(".animation1", {backgroundImage:'url(/assets/fireworks.jpg'});
+    tl.to(".animation1", {opacity: .3, scale:.6, rotation:3.01, duration: 8});
 
-    var timeline = new TimelineMax();
-    var tween1 = TweenMax.to("#al", {left: "50%", xPercent: -100, yPercent: -50});
-    var tween2 = TweenMax.to("#nat", {left: "50%", xPercent: -40, yPercent: -60});
-    var tween3 = TweenMax.to("#al, #nat", {opacity: 0});
-    var tween33 = TweenMax.to("#end", {opacity: 1});
-    var tween4 = TweenMax.to("#end", {width: 3000, height: 3000, opacity: 0});
+    //var timeline = new TimelineMax();
+    
+   // var tween1 = TweenMax.to("#al", {left: "50%", xPercent: -100});
+    //var tween2 = TweenMax.to("#nat", {right: "50%", xPercent: 50});
+    //var tween3 = TweenMax.to("#al, #nat", {opacity: 0});
+    //var tween33 = TweenMax.to("#end", {opacity: 1});
+    //var tween4 = TweenMax.to("#end", {width: 3000, height: 3000, opacity: 0});
 
 
-    timeline.add(tween1).add(tween2).add(tween3).add(tween33).add(tween4);
+    //timeline.add(tween1).add(tween2).add(tween3).add(tween33).add(tween4);
 
     
     
@@ -48,7 +51,7 @@ $(document).ready(function() {
     
     .setPin(".animation1") // trigger a TweenMax.to tween
     .addIndicators({name: "1 (duration: 0)"}) // add indicators (requires plugin)
-    .setTween(timeline)
+    .setTween(tl)
     .addTo(controller);
     
    
